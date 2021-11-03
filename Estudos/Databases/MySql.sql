bef7ab6e9672c96f7ccc10bd9037793d38d5a7cc
Anderson Macedo <andy.carlos.souza@hotmail.com>
2021-11-02 21:03:38 -0300

    Altera connection string

--- Estudos/Databases/MySql.sql
+++ Estudos/Databases/MySql.sql
@@ -1,6 +1,5 @@
 /* 
-	docker run 
-		--name mysql-estudos -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_USER=admin -e MYSQL_PASSWORD=admin -e MYSQL_DATABASE=estudos -d mysql:latest
+	docker run -p 3307:3306 --name mysql-estudos -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_USER=admin -e MYSQL_PASSWORD=admin -e MYSQL_DATABASE=estudos -d mysql:latest
 */
 
 CREATE TABLE posts(
